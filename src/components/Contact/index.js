5ef0a99ac632253259dc2c9b36604c18706c5f8c
Azarill <heierms@gmail.com>
2022-08-08 18:02:35 -0500

    deploy

---
+++ src/components/Contact/index.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
+
+function ContactForm() {
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+  const { name, email, message } = formState;
+
+  const [errorMessage, setErrorMessage] = useState('');
+
+  function handleChange(event) {
+    if (event.target.name === 'email') {
+      const isValid = validateEmail(event.target.value);
+
+      if (!isValid) {
+        setErrorMessage('Your email is invalid');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!event.target.value.length) {
+        setErrorMessage(`${event.target.name} is required`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+
+    if (!errorMessage) {
+      setFormState({ ...formState, [event.target.name]: event.target.value });
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formState);
+  }
+
+  return (
+    <section>
+      <h1 data-testid="h1tag">Contact me</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label for="name">Name:</label>
+          <input
+            type="text"
+            placeholder="Name"
+            defaultValue={name}
+            onBlur={handleChange}
+            name="name"
+          />
+        </div>
+
+        <div>
+          <label for="email">Email address:</label>
+          <input
+            type="text"
+            placeholder="Email"
+            defaultValue={email}
+            onBlur={handleChange}
+            name="email"
+          />
+        </div>
+
+        <div>
+          <label for="message">Message:</label>
+          <textarea
+            defaultValue={message}
+            name="message"
+            onBlur={handleChange}
+            rows="5"
+          />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button data-testid="submit-btn" type="submit">
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+}
+
+export default ContactForm;
